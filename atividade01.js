cf30910f5ebfb4de6b9a92ef60a3454e4753d755
NaoEoJV <gameover360xb@gmail.com>
2024-12-13 19:20:43 -0300

    at

---
+++ atividade01.js
@@ -0,0 +1,7 @@
+//declare uma variavel para armazenar um nome de um aluno, idade, 
+//
+let nome = "João vitor";
+let idade = 20;
+let notaFinal = 10;
+
+console.log("nome", nome, "idade", idade, "notaFinal", notaFinal );
